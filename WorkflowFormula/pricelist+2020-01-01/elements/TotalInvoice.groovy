09c23745aa93bf1b20eca23702dfa16e62011f86
Óscar <osga96@gmail.com>
2024-08-23 13:48:51 +0200

    Week 5 Lab 5 Pricelist workflow

---
+++ WorkflowFormula/pricelist+2020-01-01/elements/TotalInvoice.groovy
@@ -0,0 +1,45 @@
+/**
+ * Calculates expected profitability ( Margin % ) of the given Price List Item (PLI)
+ *
+ * @param pli Object (as Map) of Pricelist Line Item (i.e. single row of the Pricelist)
+ * @return the calculated expected profitability ( Margin % ) of the line
+ */
+def getMarginPct(pli) {
+    def basePrice = pli.calculationResults?.find { it.resultName == "BasePrice" }?.result
+    def listPrice = pli.calculationResults?.find { it.resultName == "ListPrice" }?.result
+    def marginPct = null
+    if (listPrice && basePrice != null) marginPct = (listPrice - basePrice) / listPrice
+    return marginPct
+}
+api.stream("PLI", null, Filter.equal("pricelistId", pricelist.id))?.withCloseable { iterator ->
+    iterator.each { pli ->
+        def marginPct = getMarginPct(pli)
+        def businessUnit = api.product("BusinessUnit", pli.sku)
+
+        def filters = [Filter.equal("name", businessUnit)]
+        def priceListApprovalLevel = api.findLookupTableValues("PriceListApprovalLevels", *filters)
+
+        if (priceListApprovalLevel != null && priceListApprovalLevel.size() > 0) {
+            def threshold = priceListApprovalLevel[0]?.attribute1
+
+            api.trace(marginPct)
+            api.trace(threshold)
+            api.trace("-----")
+
+            if (threshold != null) {
+                if (marginPct < threshold) {
+                    workflow.addApprovalStep("Product Manager")
+                            .withReasons("Total Invoice Price > " +
+                                    (threshold as String) + "EUR")
+                            .withUserGroupApprovers("ProductManager")
+                            .withMinApprovalsNeeded(1)
+                }
+            }
+        }
+
+    }
+}
+
+workflow.addWatcherStep("Pricing  Manager")
+        .withUserGroupWatchers("PricingManager")
+        .withReasons("PricingManager needs to approve this quote")
